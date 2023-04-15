1ff464be3f24d860b8dab07c7dadbaa5918d6500
Rye <huanghaiyun_at@163.com>
2023-04-15 17:40:42 +0800

    Update a method to plot the percentage of the features and create a directory to store the image.

--- Indicator_Plot.py
+++ Indicator_Plot.py
@@ -2,14 +2,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 from Features_analysis import features_analysis
+from Trading_date import Trading_date
 import multiprocessing
 from Use_func_and_store_outcome import Get_outcome
 from File_processing import makedir, File_process
 import numpy as np
 import warnings
+import datetime
 
 warnings.filterwarnings('ignore')
-
+td = Trading_date()
 '''
 indicator calculated from bar data.
 '''
@@ -34,13 +36,31 @@ class Feature_plot:
             feature = f_a.winsor(fseries)
             data_list = feature.values.reshape(-1, ).tolist()
             data_all_list = data_all_list + data_list
-        #TrueRV = [x / 10000 for x in data_all_list]
-        plt.hist(x = data_all_list, bins = 100,)
+        plt.hist(x = data_all_list, bins = 100, )
         plt.title(feature_name)
         plt.savefig(feature_plot_path + feature_name + ".png")
         plt.show()
+
+    def get_percentage_change(self, feature_name, feature_path, feature_plot_path):
+        percentage_list = [10, 25, 50, 75, 90]
+        start_date = f_p.get_name_date(os.listdir(feature_path)[0])
+        end_date = f_p.get_name_date(os.listdir(feature_path)[-1])
+        percentage_df = pd.DataFrame(index = td.get_date_list_between_two_dates(start_date, end_date),
+                                     columns = [str(x) for x in percentage_list])
+        for file in os.listdir(feature_path):
+            date = f_p.get_name_date(file)
+            fseries = pd.read_csv(feature_path + file, usecols = [1]).iloc[:, 0]
+            for per in percentage_list:
+                percentage_df.loc[date, str(per)] = np.percentile(fseries, per)
+
+        percentage_df.index = pd.to_datetime(percentage_df.index)
+        percentage_df.plot()
+        plt.legend(percentage_df.columns)
+        plt.title(feature_name+": Cross-Sectional Percentiles")
+        plt.savefig(feature_plot_path + feature_name + "percentage.png")
 
 
 for feature_name in ['RV', 'RSkew', 'Rkurtosis']:
     feature_path = feature_analysis_path + feature_name + r"/"
     Feature_plot().history_distribution(feature_name, feature_path, feature_plot_path)
+    Feature_plot().get_percentage_change(feature_name, feature_path, feature_plot_path)
